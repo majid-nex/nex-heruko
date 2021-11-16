d26916bbe3b3d4b7a15cc444e850fdd43ea13fef
majid-nex <77442341+majid-nex@users.noreply.github.com>
2021-11-16 16:53:48 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
 system ("git clone https://GitHub.com/majid-nex/nextron-bot && cd nextron-bot && python3 -m userbot")
-# try af
+# try afk
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
